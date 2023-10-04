b241e031c46061046d2c16b6a831faea6d6a0ab2
hdXian <hyun353500@naver.com>
2023-10-04 19:16:58 +0900

    Feat[#5] dockefile 작성

---
+++ Internal/docker-images/pyspark-notebook/jupyter_notebook_config.py
@@ -0,0 +1,14 @@
+from notebook.services.contents.filemanager import FileContentsManager
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def pre_save_hook(model, **kwargs):
+    # Your custom pre-save action here
+    # print("Performing custom pre-save action for new notebook.")
+    logger.info("Performing custom pre-save action for new notebook.")
+
+# Register the pre-save hook
+c.FileContentsManager.pre_save_hook = pre_save_hook
