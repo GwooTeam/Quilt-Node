24e39ab69f0d209a4a6d387627b20ce89f6a9a0f
byObin <byobin26@gmail.com>
2023-12-03 01:05:52 +0900

    Feat[#35] key pair, nonce 생성 기능 구현

--- External/InternodeAuth/supplier.js
+++ External/InternodeAuth/supplier.js
@@ -1,41 +1,101 @@
 //tcp client
 const net = require('net');
+const { exec } = require('child_process');
 const readline = require('readline');
+const fs = require('fs');
+
+
+const keygen_sign = () => {
+    exec('./dmodule --keygen', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Execution error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            return;
+        }
+        console.log(`keygen_sign Output: ${stdout}`);
+    });
+};
+
+const nonce_sign = (callback) => {
+  exec('./dmodule -s nonce.txt dilithium_key.prk', (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Execution error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+          return;
+      }
+      console.log(`nonce_sign Output: ${stdout}`);
+      if (callback) {
+        callback(); // Call the callback function once execution is complete
+    }
+  });
+};
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-
 const config = require('./config.json');
-
 const port = config.server_port;
 const host = config.server_ip;
 
 const client = new net.Socket();
 
 client.connect(port, host, () => {
-  console.log('Connected to chat server');
-  rl.prompt();
-
-  rl.on('line', (line) => {
-    client.write(line);
+    console.log('Connected to chat server');
     rl.prompt();
-  });
+
+    rl.on('line', (line) => {
+        client.write(line);
+        rl.prompt();
+    });
 });
 
 client.on('data', (data) => {
-  process.stdout.write('\u001b[2K\u001b[200D'); // Clear the current line and move the cursor to the beginning
-  console.log(data.toString());
+  const message = data.toString().trim();
+  console.log(`\nServer: ${message}`);
+  // Check if the message is a random value
+  if (message.startsWith('Random Value: ')) {
+      const randomValue = message.split('Random Value: ')[1];
+      console.log('Executing printhello program...');
+      keygen_sign();
+
+      // Write the random value to a file
+      fs.writeFile('nonce.txt', randomValue, (err) => {
+          if (err) {
+              console.error(`Error writing to file: ${err}`);
+          } else {
+              console.log('Random value saved to nonce.txt');
+
+          }
+      });
+  }
   rl.prompt();
 });
 
+// Function to send a file
+function sendFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  readStream.on('open', () => {
+      readStream.pipe(client);
+  });
+  readStream.on('error', (err) => {
+      console.error(`Error reading file: ${err}`);
+  });
+}
+
+
 client.on('close', () => {
-  console.log('Connection to chat server closed');
-  process.exit(0);
+    console.log('Connection to chat server closed');
+    process.exit(0);
 });
 
 client.on('error', (err) => {
-  console.error(`Error: ${err.message}`);
+    console.error(`Error: ${err.message}`);
 });
--- External/InternodeAuth/user.js
+++ External/InternodeAuth/user.js
@@ -1,19 +1,24 @@
 //tcp server
 const net = require('net');
+const fs = require('fs');
 
 const port = 31245;
 const host = 'localhost';
 
 const server = net.createServer(socket => {
     console.log('Client connected');
-
+    let fileWriteStream;
     socket.on('data', data => {
         const message = data.toString().trim();
         if (message === 'auth request') {
             console.log('Authentication request received');
-            // Generate a random value
-            const randomValue = Math.random().toString(36).substr(2, 9);
-            socket.write(`Random Value: ${randomValue}`);
+           // Generate a random value
+           const randomValue = Math.random().toString(36).substr(2, 9);
+           console.log(`Generated Random Value: ${randomValue}`);
+
+           // Send the random value to the client
+           socket.write(`Random Value: ${randomValue}`);
+            
         }
     });
 
