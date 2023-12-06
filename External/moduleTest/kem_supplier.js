198c63e2c1feffc237f700bb74740315ecf7806e
hdXian <hyu353500@naver.com>
2023-12-06 15:32:13 +0900

    Feat[#38] 키 교환 테스트 코드 개발

---
+++ External/moduleTest/kem_supplier.js
@@ -0,0 +1,183 @@
+const net = require('net');
+const { execSync } = require('child_process');
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const config = require('./supplier_config.json');
+const textPort = config.server_text_port; // Port for text communication
+const filePort = config.server_file_port; // Port for file transfer
+const host = config.server_ip;
+
+
+/* ############### server connect ################ */
+const textClient = new net.Socket();
+
+
+textClient.connect(textPort, host, () => {
+    console.log('Connected to the text server');
+    rl.prompt();
+
+    rl.on('line', (line) => {
+        textClient.write(line);
+        rl.prompt();
+    });
+});
+
+textClient.on('data', (data) => {
+    const message = data.toString().trim();
+    console.log(`Server: ${message}`);
+
+    // Check if the message is a random value
+    if (message.startsWith('Random Value: ')) {
+        const randomValue = message.split('Random Value: ')[1];
+        handleNonceSign(randomValue);
+    }
+
+    rl.prompt();
+});
+
+textClient.on('close', () => {
+    console.log('Connection to the text server closed');
+    rl.close();
+});
+
+textClient.on('error', (err) => {
+    console.error(`Error: ${err.message}`);
+});
+
+
+/* ##################### functions ####################### */
+
+
+function kem_encapsulate(pukPath, result_path) {
+    execSync(`../KEM/modules/kmodule -f --encap --key=${pukPath} --result=${result_path}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Execution error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            return;
+        }
+        console.log(`kem_encapsulate Output: ${stdout}`);
+    });
+}
+
+
+function ssk_decrypt(ssk_path, file_path, result_path) {
+    var res = execSync(`../KEM/modules/kmodule -f --decrypt --key=${ssk_path} --target=${file_path} --result=${result_path}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Execution error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            return;
+        }
+        console.log(`ssk_decrypt Output: ${stdout}`);
+    });
+
+    console.log(res.toString());
+}
+
+
+const fileClient = new net.Socket();
+
+function sendFile(filePath, callback) {
+
+    fileClient.connect(filePort, host, () => {
+        console.log(`Connected to the file server for sending ${filePath}`);
+        const fileContent = fs.readFileSync(filePath);
+
+        fileClient.write(fileContent);
+        fileClient.end();
+    });
+}
+
+
+let receivedData = Buffer.from([]);
+fileClient.on('data', (data) => {
+    receivedData = Buffer.concat([receivedData, data]);
+});
+
+
+fileClient.on('end', () => {
+    // 서버에서 받은 작업 완료된 파일을 클라이언트에 저장
+    const filename = 'suppResource/received_encrypted.bin';
+    fs.writeFileSync(filename, receivedData);
+    console.log('File transfer completed');
+
+    // 소켓 연결 종료
+    fileClient.destroy();
+
+    const ssk_path = 'suppResource/kyber_sharedSecret.ssk';
+    const enc_path = 'suppResource/received_encrypted.bin';
+    const result_path = 'suppResource/';
+    console.log('line 131');
+
+    ssk_decrypt(ssk_path, enc_path, result_path);
+    
+})
+
+
+
+/* ############### send file ############### */
+const puk_path = 'suppResource/kyber_key.puk';
+const capsule_save_path = 'suppResource/';
+const capsule_file_path = 'suppResource/kyber_encapsulated.cap';
+
+async function runFunctions() {
+    console.log('capsulation start: ' + (new Date()).getTime());
+    await kem_encapsulate(puk_path, capsule_save_path);
+    await sendFile(capsule_file_path);
+}
+
+runFunctions();
+
+// kem_encapsulate(puk_path, capsule_save_path)
+
+// sendFile(capsule_file_path);
+
+
+// function handleNonceSign(randomValue) {
+//     console.log('Executing keygen_sign...');
+//     keygen_sign();
+
+//     console.log('Saving random value to nonce.txt and executing nonce_sign...');
+//     fs.writeFile('nonce.txt', randomValue, (err) => {
+//         if (err) {
+//             console.error(`Error writing to file: ${err}`);
+//             return;
+//         }
+//         nonce_sign(() => {
+//             sendFile('dilithium_signed.bin', () => {
+//               sendFile('dilithium_key.puk');
+//             }); // Replace 'exampleFile.txt' with your actual file name
+//         });
+//     });
+// }
+
+
+// function nonce_sign(callback) {
+//     exec('./dmodule -s nonce.txt dilithium_key.prk', (error, stdout, stderr) => {
+//         if (error) {
+//             console.error(`Execution error: ${error.message}`);
+//             return;
+//         }
+//         if (stderr) {
+//             console.error(`Stderr: ${stderr}`);
+//             return;
+//         }
+//         console.log(`nonce_sign Output: ${stdout}`);
+//         if (callback) {
+//             callback();
+//         }
+//     });
+// }
+
+
---
+++ External/moduleTest/kem_user.js
@@ -0,0 +1,160 @@
+//tcp server
+const net = require('net');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+
+/* ################ server conntect ################# */
+const text_port = 31245;
+const file_port = 31246;
+const host = 'localhost';
+
+const socket = new net.Socket();
+
+let fileCount = 0; // To keep track of the number of files received
+
+const textServer = net.createServer(socket => {
+    console.log('Client connected');
+    socket.on('data', data => {
+        const message = data.toString().trim();
+        if (message === 'auth request') {
+            console.log('Authentication request received');
+           // Generate a random value
+           const randomValue = Math.random().toString(36).substr(2, 9);
+           console.log(`Generated Random Value: ${randomValue}`);
+
+           // Send the random value to the client
+           socket.write(`Random Value: ${randomValue}`);
+          }
+        }
+      )
+}); // const textServer
+
+
+socket.on('end', () => {
+  console.log('Client disconnected');
+});
+
+socket.on('error', (error) => {
+  console.error(`Error: ${error.message}`);
+});
+
+textServer.listen(text_port, host, () => {
+    console.log(`Text Server listening at ${host}:${text_port}`);
+});
+
+
+// File transfer server
+const fileServer = net.createServer(socket => {
+  console.log('File transfer connection established');
+  let fileWriteStream;
+
+  // capsule 데이터를 받았을 때
+  socket.on('data', (data) => {
+
+    // capsule 데이터 파일로 저장
+    if (!fileWriteStream) {
+        // Determine the filename based on the order of files received
+        const filename = 'userResource/received_capsule.cap';
+        fs.writeFileSync(filename, data);
+        // fileWriteStream = fs.createWriteStream(filename);
+        console.log(`Receiving file: ${filename}`);
+    }
+    // console.log('before await');
+    
+    // fileWriteStream.write(data);
+
+    const prk_path = 'userResource/kyber_key.prk';
+    const capsule_path = 'userResource/received_capsule.cap';
+    const result_path = 'userResource/';
+    const ssk_path = 'userResource/kyber_sharedsecret.ssk';
+    const data_path = 'userResource/testData.txt';
+    const encryptFilePath = 'userResource/kyber_encrypted.bin';
+    // console.log('before decap')
+    // capslue 파일을 prk로 디캡슐화 하고 암호화 파일을 생성한다.
+
+    kem_decapsulate(prk_path, capsule_path, result_path);
+
+    ssk_encrypt(ssk_path, data_path, result_path);
+
+
+  }); // socket.on('data')
+
+  socket.on('end', () => {
+    const encryptFilePath = 'userResource/kyber_encrypted.bin';
+    const content = fs.readFileSync(encryptFilePath);
+    socket.write(content);
+    console.log('data transfer end: ' + (new Date()).getTime());
+    console.log('File transfer completed');
+    socket.end();
+  });
+
+  socket.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+  });
+
+
+}); // const fileServer
+
+
+fileServer.listen(file_port, 'localhost', () => {
+  console.log(`File server listening at ${host}:${file_port}`);
+});
+
+
+
+/* #################### functions ####################### */
+
+
+function kem_decapsulate(prk_path, capsule_path, res_path) {
+  console.log('into decapsulte.')
+  var res = execSync(`../KEM/modules/kmodule -f --decap --key=${prk_path} --target=${capsule_path} --result=${res_path}`, (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Execution error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+          return;
+      }
+      console.log(`decapsulate Output: ${stdout.toString()}`);
+  });
+
+  // console.log(res.toString());
+  
+}
+
+
+
+function ssk_encrypt(ssk_path, data_path, result_path) {
+  console.log('into encrypt.')
+  execSync(`../KEM/modules/kmodule -f --encrypt --key=${ssk_path} --target=${data_path} --result=${result_path}`, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Execution error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+        return;
+    }
+    console.log(`decrypt Output: ${stdout}`);
+    
+});
+
+}
+
+// const prk_path = 'userResource/kyber_key.prk';
+// const capsule_path = 'userResource/received_capsule.cap';
+// const result_path = 'userResource/';
+// const ssk_path = 'userResource/kyber_sharedsecret.ssk';
+// const data_path = 'userResource/testData.txt';
+// const encryptFilePath = 'userResource/kyber_encrypted.bin';
+
+// async function runFunctions() {
+//   await kem_decapsulate(prk_path, capsule_path, result_path);
+//   await ssk_encrypt(ssk_path, data_path, result_path);
+// }
+
+
+
+
