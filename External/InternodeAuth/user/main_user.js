5ce14a240599b53f6891962ec62365dfb7c9fc98
byObin <byobin26@gmail.com>
2023-12-07 03:20:54 +0900

    Feat[#44] main_user.js와 main_supplier.js에 DSA 코드 추가

--- External/InternodeAuth/user/main_user.js
+++ External/InternodeAuth/user/main_user.js
@@ -1,9 +1,25 @@
 //user's main code
 const { spawn } = require('child_process');
 
-// verify code
+/*//////////ML-DSA(digital signature) code ////////*/
 
-// kem code
+// verify code
+const user_dsa = spawn('node', ['user.js']);
+
+// 콘솔에 출력 표시
+user_dsa.stdout.on('data', (data) => {
+    console.log(`user_dsa output: ${data}`);
+});
+
+// 종료 이벤트 처리
+user_dsa.on('close', (code) => {
+    console.log(`user_dsa process exited with code ${code}`);
+});
+
+
+
+
+///////////// kem code/////////////
 const user_kem = spawn('node', ['kem_user.js']);
 
 
--- External/InternodeAuth/user/user.js
+++ External/InternodeAuth/user/user.js
@@ -9,9 +9,9 @@ const process = require('process');
 const config = require('./config.json');
 const host = config.supplier_ip;
 const textPort = config.supplier_txt_port;
-const filePort = config.supplier_file_port; 
+const filePort = config.supplier_file_port;
 
-let fileReceivedCount = 0; 
+let fileReceivedCount = 0;
 
 
 const socket = new net.Socket();
@@ -19,89 +19,92 @@ const socket = new net.Socket();
 
 const textServer = net.createServer(socket => {
     console.log('Client connected');
+    console.time("authModuleExecutionTime");
+    // Generate a random value
+    const randomValue = Math.random().toString(36).substr(2, 9);
+    // console.log(`Generated Random Value: ${randomValue}`);
+
+    // Send the random value to the client
+    socket.write(`Random Value: ${randomValue}`);
+
+    // Save the random value to a file
+    fs.writeFile('nonce.txt', randomValue, err => {
+        if (err) {
+            console.error(`Error writing to file: ${err}`);
+        } else {
+            // console.log('Nonce saved to nonce.txt');
+        }
+    });
+
     socket.on('data', data => {
-        const message = data.toString().trim();
-        if (message === 'auth request') { 
-            console.log('Authentication request received');
-            console.time("authModuleExecutionTime");
-           // Generate a random value
-           const randomValue = Math.random().toString(36).substr(2, 9);
-           console.log(`Generated Random Value: ${randomValue}`);
+        // const message = data.toString().trim();
+        // if (message === 'auth request') { 
 
-           // Send the random value to the client
-           socket.write(`Random Value: ${randomValue}`);
+    }
 
-           // Save the random value to a file
-           fs.writeFile('nonce.txt', randomValue, err => {
-            if (err) {
-                console.error(`Error writing to file: ${err}`);
-            } else {
-                console.log('Nonce saved to nonce.txt');
-            }
-        });
-        }
-        }
-      )
-    });
+    )
+});
 
-  socket.on('end', () => {
-        console.log('Client disconnected');
-    });
+socket.on('end', () => {
+    console.log('Client disconnected');
+});
 
-  socket.on('error', (error) => {
-      console.error(`Error: ${error.message}`);
-    });
+socket.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+});
 
 textServer.listen(textPort, host, () => {
-    console.log(`Server listening at ${host}:${textPort}`);
+    // console.log(`Server listening at ${host}:${textPort}`);
 });
 
 
 // File transfer server
 const fileServer = net.createServer(socket => {
-  console.log('File transfer connection established');
-  let fileWriteStream;
-  socket.on('data', data => {
-    if (!fileWriteStream) {
-        const filename = fileReceivedCount === 0 ? 'received_dilithium_key.puk' : 'received_signed.bin';
-        fileWriteStream = fs.createWriteStream(filename);
-        console.log(`Receiving and saving to ${filename}`);
-    }
-    fileWriteStream.write(data);
-});
+    // console.log('File transfer connection established');
+    
+    let fileWriteStream;
+    socket.on('data', data => {
+        if (!fileWriteStream) {
+            const filename = fileReceivedCount === 0 ? 'received_dilithium_key.puk' : 'received_signed.bin';
+            fileWriteStream = fs.createWriteStream(filename);
+            // console.log(`Receiving and saving to ${filename}`);
+        }
+        fileWriteStream.write(data);
+    });
 
-  socket.on('end', () => {
-      fileWriteStream.end();
-      console.log('File transfer completed');
-      fileReceivedCount++;
+    socket.on('end', () => {
+        fileWriteStream.end();
+        // console.log('File transfer completed');
+        fileReceivedCount++;
 
-      if (fileReceivedCount === 2) { // 두 번째 파일 수신 후 verify 실행
-          nonce_verify();
-      }
-  });
+        if (fileReceivedCount === 2) { // 두 번째 파일 수신 후 verify 실행
+            nonce_verify();
+        }
+    });
 
-  socket.on('error', error => {
-      console.error(`Error: ${error.message}`);
-  });
+    socket.on('error', error => {
+        console.error(`Error: ${error.message}`);
+    });
 });
 
 fileServer.listen(filePort, host, () => {
-  console.log('File server listening on port 31246');
+    // console.log('File server listening on port 31246');
 });
 
 function nonce_verify() {
-  exec('node verify.js', (error, stdout, stderr) => {
-      if (error) {
-          console.error(`Execution error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.error(`Stderr: ${stderr}`);
-          return;
-      }
-      console.log(`nonce_verify Output: ${stdout}`);
-      console.timeEnd("authModuleExecutionTime");
-      
-      process.exit(0);
-  });
+    exec('node verify.js', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Execution error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            return;
+        }
+        console.log(`nonce_verify Output: ${stdout}`);
+        console.timeEnd("authModuleExecutionTime");
+        fileReceivedCount = 0;
+        // process.exit(0);
+        return;
+    });
 }
