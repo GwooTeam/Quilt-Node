b86ebd1952d40ec49b403d5d498d3129c7506ac5
hdXian <hyun353500@naver.com>
2023-12-11 05:44:29 +0900

    Fix[#38] kmodule 암호화 원본 데이터 길이 출력 추가

--- crypto/kmodule/src/run.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-
-gcc -m64 -D_LINUX -o ../modules/kmodule *.c -L../modules -lNSKem #  && ./run
-
-# rm run
- 
---
+++ crypto/mmodule/modules/compile.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+gcc -m64 -D_LINUX -o mmodule ../src/*.c -L. -lNSMac #  && ./run
+
+# rm run
+ 
